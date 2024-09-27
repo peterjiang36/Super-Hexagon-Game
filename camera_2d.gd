030a4e065d0498b081dea7b92c5bce829accaedd
Peter Jiang <peter.j@usask.ca>
2024-09-27 16:45:13 -0600

    camera rotation adjustment
    slower down the camera rotation

--- camera_2d.gd
+++ camera_2d.gd
@@ -1,6 +1,6 @@
 extends Camera2D
 
-@export var rotation_speed = 1  # Adjust this value to control the rotation speed
+@export var rotation_speed = 0.6  # Adjust this value to control the rotation speed
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
